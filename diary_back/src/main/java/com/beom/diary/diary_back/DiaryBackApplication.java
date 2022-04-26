d0d3a914b4ea8b878da090c9d027549822df160e
beombeom1119 <qjarl1119@naver.com>
2022-04-26 13:53:59 +0900

    first commit

---
+++ diary_back/src/main/java/com/beom/diary/diary_back/DiaryBackApplication.java
@@ -0,0 +1,13 @@
+package com.beom.diary.diary_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryBackApplication.class, args);
+	}
+
+}
---
+++ diary_back/src/test/java/com/beom/diary/diary_back/DiaryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beom.diary.diary_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
