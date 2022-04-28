3c1b7774da4bfbb4a3b6aa29c8c17ac069c04bf3
beombeom1119 <qjarl1119@naver.com>
2022-04-28 19:59:02 +0900

    "HomPage" 변경

--- diary_front/src/components/layouts/Content.js
+++ diary_front/src/components/layouts/Content.js
@@ -1,26 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Create from '../Create'
-import Home from '../Home'
-import Footer from './Footer'
-import Header from './Header'
-import MyPage from './MyPage'
+
 
 
 const Content = () => {
   return (
-    <div className='Content'>
-    <BrowserRouter>
-    <Header></Header>
-    <Routes>
-    <Route exact path="/" element={<Home></Home>} />
-    {/* <Route path="/mypage" element= {<MyPage></MyPage>}></Route> */}
-    <Route path="/create" element= {<Create></Create>}/>
-    <Route path="/mypage" element= {<MyPage></MyPage>}/>
-    </Routes>
-    <Footer></Footer>
-    </BrowserRouter>
-    </div>
+
   )
 }
 
